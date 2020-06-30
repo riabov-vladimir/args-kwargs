2ee5cbe0600fd3112462b35f74bfbf2b164b42cd
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-06-30 13:08:41 +0300

    remove contact - done

--- application/phone_book.py
+++ application/phone_book.py
@@ -6,8 +6,7 @@ from application.class_contact import Contact
 Телефонная книга должна работать с классами Contact.
 Методы:
 
-Вывод контактов из телефонной книги;
-Добавление нового контакта;
+
 Удаление контакта по номеру телефона;
 Поиск всех избранных номеров;
 Поиск контакта по имени и фамилии.
@@ -24,13 +23,19 @@ class PhoneBook:
 	def add_contact(self, contact):
 		self.contacts.append(contact)
 
+	def remove_contact(self, phone_number):
+		for contact in self.contacts:
+			if contact.phone_umber == phone_number:
+				self.contacts.remove(contact)
 
 if __name__ == '__main__':
-	c_list = ('1', '2', '3')     # [Contact('a', 'b', 123), Contact('c', 'd', 456)]
+
 	my_phonebook = PhoneBook(Contact('a', 'b', 123, telegram=325325), Contact('c', 'd', 456))
-	my_phonebook.show_contacts()
-	print('')
+	# my_phonebook.show_contacts()
+	print(my_phonebook.contacts)
 	my_phonebook.add_contact(Contact('e', 'f', 789))
 	# my_phonebook.show_contacts()
 	print(my_phonebook.contacts)
-	my_phonebook.show_contacts()
+	# my_phonebook.show_contacts()
+	my_phonebook.remove_contact(789)
+	print(my_phonebook.contacts)
